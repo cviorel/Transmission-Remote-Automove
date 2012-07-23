9f34f8100c716d1a3c70ee6b8d0ff420f22c249f
Jesse L. Overton <jovrtn@gmail.com>
2012-07-23 15:05:15 -0800

    variable fixes

--- torrents-completed.sh
+++ torrents-completed.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
-
-# script to check for complete torrents in transmission folder, then stop and move them
+# Description:
+# Checks for complete torrents in transmission folder, stops & moves them
 
 MOVEDIR=~/torrents/completed
-RATIOLIMIT = `transmission-remote -si | grep "Default seed ratio limit:" | cut -d \: -f 2`
+
+# get ratio limit value
+RATIOLIMIT=`transmission-remote -si | grep "Default seed ratio limit:" | cut -d \: -f 2`
 
 # get torrent list from transmission-remote list
 # delete first / last line of output
@@ -14,25 +16,29 @@ TORRENTLIST=`transmission-remote --list | sed -e '1d;$d;s/^ *//' | cut -s -d " "
 # for each torrent in the list
 for TORRENTID in $TORRENTLIST
 do
-  echo "* * * * * Operations on torrent ID $TORRENTID starting. * * * * *"
-
-  # check if torrent was started
-  STARTED=`transmission-remote --torrent $TORRENTID --info | grep "Id: $TORRENTID"`
-  # echo " - started state = $STARTED" # debug message
-
-  # check if torrent download is completed
-  COMPLETED=`transmission-remote --torrent $TORRENTID --info | grep "Percent Done: 100%"`
-  # echo " - completed state = $COMPLETED" # debug message
-
-  # check torrent's current state is "Stopped"
-  STOPPED=`transmission-remote --torrent $TORRENTID --info | grep "State: Finished"`
-  # echo " - torrent stopped seeding = $STOPPED" # debug message
-  
-  # check torrent's current ratio is "1.50"
-  CAPPED=`transmission-remote --torrent $TORRENTID --info | grep "Ratio: $RATIOLIMIT"`
+    echo "* * * * * Operations on torrent ID $TORRENTID starting. * * * * *"
+    
+    # check if torrent was started
+    STARTED=`transmission-remote --torrent $TORRENTID --info | grep "Id: $TORRENTID"`
+    # echo " - started state = $STARTED" # debug message
+    
+    # check if torrent download is completed
+    COMPLETED=`transmission-remote --torrent $TORRENTID --info | grep "Percent Done: 100%"`
+    # echo " - completed state = $COMPLETED" # debug message
+    
+    # check torrent's current state is "Stopped"
+    STOPPED=`transmission-remote --torrent $TORRENTID --info | grep "State: Finished"`
+    # echo " - torrent stopped seeding = $STOPPED" # debug message
+    
+    # check to see if ratio-limit-enabled is true
+    if [ "$RATIOLIMIT" != "Unlimited" ]; then
+        # check if torrent's ratio matches ratio-limit
+        CAPPED=`transmission-remote --torrent $TORRENTID --info | grep "Ratio: $RATIOLIMIT"`
+    fi
 
   # if the torrent is "Stopped" after downloading 100% and seeding, move the files and remove the torrent from Transmission
-  if [ "$STARTED" != "" ] && [ "$COMPLETED" != "" ] && [ "$STOPPED" != "" ] && [ "$CAPPED" != "" ]; then
+  
+  if  [ "$STARTED" != "" ] && [ "$COMPLETED" != "" ] && [ "$STOPPED" != "" ] && { [ "${CAPPED+x}" = x ] && [ -z "$CAPPED" ]; }; then
     echo "Torrent #$TORRENTID is completed."
     echo "Moving downloaded file(s) to $MOVEDIR."
     transmission-remote --torrent $TORRENTID --move $MOVEDIR
